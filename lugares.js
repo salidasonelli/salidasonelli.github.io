e62b2d72ad849f0fa0ccf760ab97de60ebe2a4fd
onelli <congreonelli@gmail.com>
2026-07-10 20:16:24 -0300

    Update lugares.js

--- lugares.js
+++ lugares.js
@@ -1,1 +1,13 @@
+const lugares = {
 
+    diagGutierrez: {
+        nombre: "Diag. Gutiérrez y Castex",
+        maps: "https://maps.google.com/?q=Diag.+Gutiérrez+y+Castex,+San+Carlos+de+Bariloche"
+    },
+
+    dosDeAgosto: {
+        nombre: "2 de Agosto y John O'Connor",
+        maps: "https://maps.google.com/?q=2+de+Agosto+y+John+O'Connor,+San+Carlos+de+Bariloche"
+    }
+
+};
