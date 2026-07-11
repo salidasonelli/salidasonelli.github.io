eb40799a842296a038bf39bfda339b7fd1efe1c5
onelli <congreonelli@gmail.com>
2026-07-10 21:11:37 -0300

    Update lugares.js

--- lugares.js
+++ lugares.js
@@ -1,5 +1,10 @@
 const lugares = {
 
+    fliaHueraman: {
+        nombre: "Flia. Hueraman",
+        maps: "https://maps.app.goo.gl/V4CBrHLQq9ijNWdf6"
+    },
+    
     castexGutierrez: {
         nombre: "Diag. Gutiérrez y Castex",
         maps: "https://maps.google.com/?q=Diag.+Gutiérrez+y+Castex,+San+Carlos+de+Bariloche"
@@ -8,6 +13,31 @@ const lugares = {
     onelliBrown: {
         nombre: "Onelli y Brown",
         maps: "https://maps.app.goo.gl/FhaYmMqACuBJLif99"
+    },
+
+    julioBrown: {
+        nombre: "9 de Julio y Brown",
+        maps: "https://maps.app.goo.gl/2KiHE6Ekbdm3qQah8"
+    },
+
+    rivadariaBrown: {
+        nombre: "Rivadavia y Brown",
+        maps: "https://maps.app.goo.gl/pzh1Cwrp9tUm1NPY8"
+    },
+
+    lapazOnelli: {
+        nombre: "Onelli y La Paz",
+        maps: "https://maps.app.goo.gl/byoe3hLpj2ngzaU36"
+    },
+
+    agostoBeschtedt: {
+        nombre: "Beschtedt y 2 de Agosto",
+        maps: "https://maps.app.goo.gl/jA3vSQa4ok4QVfVe7"
+    },
+
+    mascardiBeschtedt: {
+        nombre: "Beschtedt y Mascardi",
+        maps: "https://maps.app.goo.gl/raPNQR3yiqXHiBAVA"
     },
     
     agostoOconnor: {
